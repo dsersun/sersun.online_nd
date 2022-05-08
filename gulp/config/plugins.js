2f5af2f83fcce5e0e29e46fc457050b94e677c6a
Denis Sersun <denis@sersun.online>
2022-05-08 17:40:22 +0300

    ok сервер, работа с scss/css, versioncontrol ...

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,13 @@
 import replace from "gulp-replace" // Поиск и замена
+import plumber from "gulp-plumber" // Обработка ошибок
+import notify from "gulp-notify" // Сообщения (подсказки)
+import browsersync from "browser-sync" // Сообщения (подсказки)
+
 
 // Экспорт обьекта
 export const plugins = {
-    replace: replace
+    replace: replace,
+    plumber: plumber,
+    notify: notify,
+    browsersync: browsersync
 }
--- gulpfile.js
+++ gulpfile.js
@@ -16,16 +16,19 @@ global.app = {
 import { copy } from "./gulp/task/copy.js";
 import { reset } from "./gulp/task/reset.js";
 import { html } from "./gulp/task/html.js";
+import { server } from "./gulp/task/server.js";
+import { scss } from "./gulp/task/scss.js";
 
 // Наблюдатель за изменеиниями в файлах
 function watcher() {
     gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
+    gulp.watch(path.watch.scss, scss);
 }
 
-const mainTascks = gulp.parallel(copy, html);
+const mainTascks = gulp.parallel(copy, html, scss);
 // построение сценария выполнения задач
-const dev  = gulp.series(reset, mainTascks, watcher);
+const dev  = gulp.series(reset, mainTascks, gulp.parallel(watcher,server));
 
 
 // Выполнение сценария по умолчанию
